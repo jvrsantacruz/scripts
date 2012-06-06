6396f596efc7f6228c07739521e11c0e049ddce8
Javier Santacruz <javier.santacruz.lc@gmail.com>
2012-06-06 15:09:14 +0200

    mp3hash: Adds TaggedFile class which replaces functions

--- mp3hash/mp3hash.py
+++ mp3hash/mp3hash.py
@@ -131,6 +131,144 @@ def startbyte(ofile):
     else:
         return 0
 
+class TaggedFile(object):
+
+    attrs = ('has_id3v1', 'has_id3v1ext', 'id3v1_size', 'id3v1ext_size',
+             'id3v1_totalsize', 'has_id3v2', 'has_id3v2ext', 'id3v2_size',
+             'id3v2ext_size', 'id3v2_totalsize', 'startbyte', 'endbyte',
+             'musiclimits')
+
+    def __init__(self, path):
+        self.path = path
+        self.taginfo = None
+        self.file = None
+
+    def __getattribute__(self, key):
+        """Returns cached version for properties listed in self.attrs
+        Lazy initialize self.taginfo when accesing any property the first time
+        Avoids calling self.attrs properties with self.file being None
+        """
+        if key in object.__getattribute__(self, 'attrs'):
+            if self.taginfo is None:
+                self.__getinfo()
+            return self.taginfo[key]
+
+        return object.__getattribute__(self, key)
+
+    def __getinfo(self):
+        """Calculates and returns taginfo dict
+        taginfo dict caches TaggedFile info
+        """
+        self.file = open(self.path, 'rb')
+        # taginfo = {'has_id3v1': None, ..
+        self.taginfo = dict(zip(self.attrs, (None,) * len(self.attrs)))
+
+        for attr in self.attrs:
+            self.taginfo[attr] = object.__getattribute__(self, attr)
+
+        self.file.close()
+        self.file = None
+        return self.taginfo
+
+    @property
+    def has_id3v1(self):
+        "Returns True if the file is id3v1 tagged"
+        self.file.seek(-128, 2)
+        return self.file.read(3) == 'TAG'
+
+    @property
+    def has_id3v1ext(self):
+        "Returns True if the file is id3v1 with extended tag"
+        self.file.seek(-(227 + 128), 2)  # 227 before regular tag
+        return self.file.read(4) == 'TAG+'
+
+    @property
+    def id3v1ext_size(self):
+        "Returns the size of the extended tag if exists"
+        if self.has_id3v1ext:
+            return 227
+        return 0
+
+    @property
+    def id3v1_size(self):
+        "Returns the size in bytes of the id3v1 tag"
+        if self.has_id3v1:
+            return 128
+        return 0
+
+    @property
+    def id3v1_totalsize(self):
+        "Returns the size in bytes of the id3v1 tag"
+        return self.id3v1_size + self.id3v1ext_size
+
+    @property
+    def has_id3v2(self):
+        "Returns True if the file is id3v2 tagged"
+        self.file.seek(0)
+        return self.file.read(3) == 'ID3'
+
+    @property
+    def has_id3v2ext(self):
+        "Returns True if the file has id3v2 extended header"
+        self.file.seek(5)
+        flags, = struct.unpack('>b', self.file.read(1))
+        return bool(flags & 0x40)  # xAx0 0000 get A from byte
+
+    @property
+    def id3v2_size(self):
+        "Returns the size in bytes of the id3v2 tag"
+        if not self.has_id3v2:
+            return 0
+
+        self.file.seek(6)
+        # id3v2 size big endian 4 bytes
+        size, = struct.unpack('>i', self.file.read(4))
+        size += 10  # header itself
+        return size
+
+    @property
+    def id3v2ext_size(self):
+        "Returns the size in bytes of the id3v2 extended tag"
+        if not self.has_id3v2ext:
+            return 0
+
+        self.file.seek(self.id3v2_size)
+        size = struct.unpack('>i', self.file.read(4))
+        flags, = struct.unpack('>bb', self.file.read(2))
+        crc = 4 if flags & 8 else 0  # flags are A000 get A
+        padding = struct.upnack('>i', self.file.read(4))
+        return size + crc + padding + 10
+
+    @property
+    def id3v2_totalsize(self):
+        "Returns the total size of the id3v2 tag"
+        return self.id3v2_size + self.id3v2ext_size
+
+    @property
+    def startbyte(self):
+        "Returns the byte where the music starts in file"
+        return self.id3v2_totalsize
+
+    @property
+    def endbyte(self):
+        "Returns the last byte of music data in file"
+        size = self.id3v1_totalsize
+        if not size:
+            self.file.seek(0, 2)
+            return self.file.tell()
+
+    @property
+    def musiclimits(self):
+        "Returns the (start, end) for music in file"
+        return (self.startbyte, self.endbyte)
+
+    def hash(self, alg='sha1'):
+        """Returns the hash for a certain audio file ignoring tags
+        Non cached function. Calculates the hash each time it's called
+        """
+        with open(self.path, 'rb') as ofile:
+            start, end = self.musiclimits
+            return hashfile(ofile, start, end, alg)
 
 def endbyte(ofile):
     "Returns the last byte of music"
